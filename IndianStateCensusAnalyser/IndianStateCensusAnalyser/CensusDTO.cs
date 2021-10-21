1475eafd06b394dbe965e7767697a33db9c5d807
umamahale <umagasmos@gmail.com>
2021-10-21 08:29:44 -0400

    irst message

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/CensusDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser.DTO
+{
+    class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int Pin;
+        public string stateCode;
+        public long population;
+        public long area;
+        public long density;
+        public long housingUnit;
+        public double totalArea;
+        public double waterArea;
+        public double landArea;
+        public double populationDensity;
+        public double housingDensity;
+        public CensusDTO(StateCodeDAO stateCodeDao)
+        {
+            this.serialNumber = stateCodeDao.serialNumber;
+            this.stateName = stateCodeDao.stateName;
+            this.Pin = stateCodeDao.Pin;
+            this.stateCode = stateCodeDao.stateCode;
+        }
+        public CensusDTO(CensusDataDAO censusDataDao)
+        {
+            this.state = censusDataDao.state;
+            this.population = censusDataDao.population;
+            this.area = censusDataDao.area;
+            this.density = censusDataDao.density;
+
+        }
+        public CensusDTO(USCensusDAO usCensusDao)
+        {
+            this.stateCode = usCensusDao.stateId;
+            this.stateName = usCensusDao.stateName;
+            this.population = usCensusDao.population;
+            this.housingUnit = usCensusDao.housingUnit;
+            this.totalArea = usCensusDao.totalArea;
+            this.waterArea = usCensusDao.waterArea;
+            this.landArea = usCensusDao.landArea;
+        }
+
+    }
+}
---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/StateCensusAnalyser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    class StateCensusAnalyser
+    {
+        public string Name;
+        public string Code;
+        public string Statename;
+        public string Serialnumber;
+        public string area;
+    }
+}
